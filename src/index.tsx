54a58a6f82d5b20def0eb2dc022fb417ab304563
子文 <wb-yzw695400@alibaba-inc.com>
2020-11-23 16:01:01 +0800

    chore(App): restore frame

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+/**
+ * PC 首页入口
+ */
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+function render() {
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+render();
